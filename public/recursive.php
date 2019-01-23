903f86827fbe4d33045672ea158ba1ec6f5bcb11
Etienne Châtaignier <etienne.chataignier@biig.fr>
2019-01-23 09:50:52 +0100

    On recursive demo, display level

--- public/recursive.php
+++ public/recursive.php
@@ -2,8 +2,10 @@
 
 function doSomethingRecursive($level): void {
     if ($level >= 0) {
+        echo $level.' ';
         doSomethingRecursive($level - 1);
     }
 }
 
 doSomethingRecursive(1000);
+echo 'ignition !';
